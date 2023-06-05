8fadcfaa814bc16af325c0014168049c0be631e0
Tebogo Tema <dime.tema@gmail.com>
2023-06-05 13:21:18 +0200

    Completed the search functionality

--- features/steps/amplify_ecommerce_steps.py
+++ features/steps/amplify_ecommerce_steps.py
@@ -10,60 +10,6 @@ logger = get_logger(logger_name=__name__)
 config_file = read_config_file(section='auth')
 
 
-@given(u'that I want to search for an invalid (nonexistant) category')
-def step_impl(context):
-    context.driver = initialize_driver()
-    log_to_allure(message="Searching for a non-existant category")
-
-
-@when(u'I navigate to the app, and search that category {category}')
-def step_impl(context, category):
-
-
-    AmplifyEcommerce(context.driver).login(
-        username=config_file['username'], 
-        password= config_file['password']
-    )
-
-    AmplifyEcommerce(context.driver).search_category(category=category)
-
-
-@then(u'I should get no products displayed')
-def step_impl(context):
-    
-    all_products = AmplifyEcommerce(context.driver).all_products_from_screen()
-
-    assert_with_allure(
-        condition=len(all_products) == 0,
-        message="There should not be any products displayed",
-        data=f"All products: {all_products}",
-    )
-
-
-@given(u'that I want to search for a valid (existant) category')
-def step_impl(context):
-    context.driver = initialize_driver()
-    log_to_allure(message="Searching for an existant category")
-
-
-@when(u'I navigate to the app, and seach for the category as the step above {category}')
-def step_impl(context, category):
-    context.execute_steps(f'When I navigate to the app, and search that category {category}')
-
-
-@then(u'I should get all necessary products for category {category_products}')
-def step_impl(context, category_products: str):
-    categories = category_products.split(',')
-    feature_categories = [item.strip() for item in categories]
-    all_products_from_web = AmplifyEcommerce(context.driver).all_products_from_screen()
-
-    assert_with_allure(
-        condition=sorted(feature_categories) == sorted(all_products_from_web),
-        message="Check if the two lists contain the same products",
-        data=f"\nWeb: {all_products_from_web} \nFeature: {feature_categories}"
-    )
-
-
 @given(u'that I want to add {items} of {product} to the cart')
 def step_impl(context, items, product):
     context.driver = initialize_driver()
@@ -206,38 +152,6 @@ def step_impl(context, product, letter):
 def step_impl(context):
     context.execute_steps("Then the button should not be clikable")
 
-
-@given(u'that I want to view the specials on the specials page')
-def step_impl(context):
-    context.driver = initialize_driver()
-    log_to_allure(message="Navigating to the specials page")
-
-
-@when(u'I login to the e-commerce')
-def step_impl(context):
-    context.execute_steps("When I login in to the e-commerce")
-
-
-@when(u'I navigate to the page')
-def step_impl(context):
-    AmplifyEcommerce(context.driver).navigate_to_specials()
-
-
-@then(u'I should be able to see the page')
-def step_impl(context):
-    AmplifyEcommerce(context.driver).screenshot(message="The specials page")
-
-
-@then(u'the url should be of format "https://host/specials"')
-def step_impl(context):
-    current_url = AmplifyEcommerce(context.driver).specials_url()
-    
-    assert_with_allure(
-        condition="specials" in current_url,
-        message="The url should contain `specials`: 'https://host/specials'",
-        data=f"The current url: {current_url}"
-    )
-    
 
 @given(u'that I want to add 5 items of product Keloggs Cornflakes')
 def step_impl(context):
